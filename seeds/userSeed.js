fdea0a55f1e19248dc0481cd870ebca120fdca9e
jbxamora <jbxamora@icloud.com>
2023-03-04 21:48:50 -0800

    updated seed to one file (trouble seeding both seperate files, reverted back to old server (ineficient to have seeds injected every time server started)

--- seeds/userSeed.js
+++ seeds/userSeed.js
@@ -1,35 +1,50 @@
 // Import the required dependencies
-const mongoose = require('mongoose');
-const User = require('../models/user');
-const Thought = require('../models/thought');
+const mongoose = require("mongoose");
+const Thought = require("../models/thought");
+const User = require("../models/user");
 
 // Connect to your MongoDB database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/ThoughtsUpDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/ThoughtsUpDB",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
 
 // Define the seed data
 const users = [
-    { username: 'user1', email: 'user1@example.com', thoughts: [], friends: [] },
-    { username: 'user2', email: 'user2@example.com', thoughts: [], friends: [] }
+    { username: "user1", email: "user1@example.com", thoughts: [], friends: [] },
+    { username: "user2", email: "user2@example.com", thoughts: [], friends: [] },
+];
+
+const thoughts = [
+    { thoughtText: "Thought 1", username: "user1", reactions: [] },
+    { thoughtText: "Thought 2", username: "user2", reactions: [] },
 ];
 
 // Create a function to seed the data
-const seedUsers = async () => {
+const seedData = async () => {
     try {
         // Remove any existing data
-        await User.deleteMany({});
         await Thought.deleteMany({});
+        await User.deleteMany({});
 
         // Create the new data
         const createdUsers = await User.create(users);
+        const createdThoughts = await Thought.create(thoughts);
 
-        console.log('Data seeding successful!');
+        // Associate the thoughts with the users
+        for (let i = 0; i < createdThoughts.length; i++) {
+            const thought = createdThoughts[i];
+            const user = createdUsers.find((user) => user.username === thought.username);
+            user.thoughts.push(thought._id);
+            await user.save();
+        }
+
+        console.log("Data seeding successful!");
     } catch (error) {
-        console.error('Error seeding data:', error.message);
+        console.error("Error seeding data:", error.message);
     } finally {
         // Disconnect from the database
         mongoose.disconnect();
@@ -37,7 +52,4 @@ const seedUsers = async () => {
 };
 
 // Call the function to seed the data
-seedUsers();
-
-
-module.exports = seedUsers;
+seedData();
